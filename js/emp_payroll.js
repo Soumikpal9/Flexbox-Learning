c68bc854879441a3072451a28dfd65509fe855a8
Soumikpal9 <soumikpal1997@gmail.com>
2020-11-18 14:01:22 +0530

    [SP]Added: Employee Payroll Object Saved To Local Storage

--- js/emp_payroll.js
+++ js/emp_payroll.js
@@ -1,16 +1,14 @@
 class EmpPayrollData {
-    constructor(...params){
-        this.name = params[0]
-        this.picture = params[1]
-        this.gender = params[2]
-        this.department = params[3]
-        this.salary = params[4]
-        this.startDate = params[5]
-        this.note = params[6]
+
+    get id(){
+        return this._id
+    }
+    set id(id){
+        this._id = id
     }
 
     get name(){
-        return this.name
+        return this._name
     }
     set name(name){
         let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$")
@@ -19,35 +17,35 @@ class EmpPayrollData {
     }
 
     get picture(){
-        return this.picture
+        return this._picture
     }
     set picture(picture){
-        this.picture = picture
+        this._picture = picture
     }
 
     get gender(){
-        return this.gender
+        return this._gender
     }
     set gender(gender){
-        this.gender = gender
+        this._gender = gender
     }
 
     get department(){
-        return this.department
+        return this._department
     }
     set department(department){
-        this.department = department
+        this._department = department
     }
 
     get salary(){
-        return this.salary
+        return this._salary
     }
     set salary(salary){
-        this.salary = salary
+        this._salary = salary
     }
 
     get startDate(){
-        return this.startDate
+        return this._startDate
     }
     set startDate(startDate){
         if(startDate <= new Date()) this._startDate = startDate
@@ -55,16 +53,16 @@ class EmpPayrollData {
     }
 
     get note(){
-        return this.note
+        return this._note
     }
     set note(note){
-        this.note = note
+        this._note = note
     }
 
     toString(){
         const options ={ year : "numeric", month : "long", day : "numeric"}
         const empDate = this.startDate === undefined ? "undefined" :
                        this.startDate.toLocaleDateString("en-US", options)
-       return "name = "+ this.name + " gender = "+ this.gender +" department = "+ this.department + " salary = "+ this.salary + "startDate = "+ this.startDate
+       return "id = "+ this.id + "name = "+ this.name + " gender = "+ this.gender +" department = "+ this.department + " salary = "+ this.salary + "startDate = "+ this.startDate
     }
 }
