4a79eb3c579c221ae6cb275c5d1ed7f774dd6810
Soumikpal9 <soumikpal1997@gmail.com>
2020-11-20 10:07:18 +0530

    [SP]Refactored: Local Storage Handled Properly

--- js/home.js
+++ js/home.js
@@ -7,8 +7,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
     localStorage.removeItem('editEmp')
 })
 
-const getEmployeePayrollDataFromStorage = () => {
-    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : []
+//Template literal ES6 feature
+const createInnerHtml = () => {
+    const headerHtml ="<th>Image</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
+    if(empPayrollList.length == 0) return
+    let innerHtml = `${headerHtml}`
+    for(const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td>
+                <img src="${empPayrollData._picture}" class="profile" alt="">
+            </td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}{</td>
+            <td>${stringifyDate(empPayrollData._startDate)}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/delete_icon.png">
+                <img name="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/edit_icon.png">
+            </td>
+        </tr>
+        `
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml
 }
 
 function getDeptHtml(deptList) {
@@ -18,54 +40,6 @@ function getDeptHtml(deptList) {
     }
 }
 
-const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-            _name: 'Soumik Pal',
-            _gender: 'Male',
-            _department: ['Engineer', 'Finance'],
-            _salary: '500000',
-            _startDate: '1 Nov 2020',
-            _note: '',
-            _id: new Date().getTime(),
-            _picture: '../assets/profile4.jpg'
-        },
-        {
-            _name: 'Sneha Palit',
-            _gender: 'Female',
-            _department: ['HR', 'Finance'],
-            _salary: '500000',
-            _startDate: '1 Nov 2020',
-            _note: '',
-            _id: new Date().getTime(),
-            _picture: '../assets/profile3.jpg'
-        }
-    ]
-    return empPayrollListLocal
-}
-
-//Template literal ES6 feature
-const createInnerHtml = () => {
-    const headerHtml ="<th>Image</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
-    if(empPayrollList.length == 0) return
-    let innerHtml = `${headerHtml}`
-    for(const empPayrollData of empPayrollList) {
-        innerHtml = `${innerHtml}
-    <tr>
-        <td>
-            <img src="${empPayrollData._picture}" class="profile" alt="">
-        </td>
-        <td>${empPayrollData._name}</td>
-        <td>${empPayrollData._gender}</td>
-        <td>${getDeptHtml(empPayrollData._department)}</td>
-        <td>${empPayrollData._salary}{</td>
-        <td>${stringifyDate(empPayrollData._startDate)}</td>
-        <td>
-            <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/delete_icon.png">
-            <img name="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/edit_icon.png">
-        </td>
-    </tr>
-    `
-    }
-    document.querySelector('#table-display').innerHTML = innerHtml
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : []
 }
--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,54 +1,105 @@
-const salary = document.querySelector('#salary')
-            const output = document.querySelector('.salary-output')
-            output.textContent = salary.value
-            salary.addEventListener('input', function(){
-                output.textContent = salary.value
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    const salary = document.querySelector('#salary')
+    const output = document.querySelector('.salary-output')
+    output.textContent = salary.value
+    salary.addEventListener('input', function(){
+      output.textContent = salary.value
+    })
+    
+    const name = document.querySelector("#name")
+    const textError = document.querySelector(".text-error")
+    name.addEventListener('input', function(){
+      if(name.value.length == 0){
+        textError.textContent = ""
+        return
+      }
+      try{
+        (new EmployeePayrollData()).name = name.value
+        textError.textContent = ""
+      }catch(e){
+        textError.textContent = e
+      }
+    })
+    
+    const startDate = document.querySelector("#startDate")
+    const day = document.querySelector("#day")
+    const month = document.querySelector("#month")
+    const year = document.querySelector("#year")
+    const dateError = document.querySelector(".date-error")
+    startDate.addEventListener("input", async function(){
+       try{
+       new EmployeePayrollData().startDate = new Date( Date.UTC(year.value, month.value - 1, day.value))
+        dateError.textContent = ""
+      }catch(e){
+        dateError.textContent = e
+      }
+    })
 })
-
-const text = document.querySelector("#name")
-const textError = document.querySelector(".text-error")
-const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$")
-text.addEventListener("input", function () {
-    if (nameRegex.test(text.value)) textError.textContent = ""
-    else textError.textContent = "Name is Incorrect"
-});
-
-const createEmpPayroll = () => {
+    
+const save = () => {
+    try{
+    let employee = saveData()
+    createAndUpdateStorage(employee)
+    }catch(e){
+        return
+    }
+}
+    
+function createAndUpdateStorage(employee){
+    let employeeList = JSON.parse(localStorage.getItem("EmployeePayrollList"))
+    if(employeeList != undefined) employeeList.push(employee)
+    else employeeList = [employee]
+    
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeeList))
+    alert(employeeList.toString())
+}
+    
+const getSelectedValues = (property) => {
+    let allItems = document.querySelectorAll(property)
+    let setItems = []
+    allItems.forEach(item => {
+    if(item.checked) setItems.push(item.value)
+    })
+    return setItems
+}
+    
+function saveData(){
     let employee = new EmployeePayrollData()
     employee.name= document.getElementById("name").value
     employee.picture = document.querySelector('input[name = profile]:checked').value
     employee.gender = document.querySelector('input[name = gender]:checked').value
-    employee.department =document.querySelector('input[name = department]:checked').value
+    employee.department =getSelectedValues('[name=department]')
     employee.salary = document.getElementById("salary").value
     var day = document.getElementById("day").value
     var month = document.getElementById("month").value
     var year = document.getElementById("year").value
     employee.note = document.getElementById("notes").value
     employee.startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
-              
-    alert("Thank you. Your data is saved" + employee.toString())
+    
     return employee
 }
-
-function createAndUpdateStorage(empPayrollData){
-    let empPayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"))
-    if(empPayrollList != undefined){
-        empPayrollList.push(empPayrollData)
-    }
-    else{
-        empPayrollList = [empPayrollData]
-    }
-    alert(empPayrollList.toString())
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList))
+    
+const setValue = (id, value) => {
+    const element = document.querySelector(id)
+    element.value = value
 }
-
-const save = () => {
-    try{
-        let empPayrollData = createEmpPayroll()
-        createAndUpdateStorage(empPayrollData)
-    }
-    catch(e){
-        return
-    }
+    
+const unsetSelectedValues = (propertyValue) =>{
+    let allItems = document.querySelectorAll(propertyValue)
+    allItems.forEach(item => {
+    item.checked = false
+    })
 }
-
+    
+const resetForm = () => {
+    setValue("#name", "")
+    unsetSelectedValues("[name=profile]")
+    unsetSelectedValues("[name=gender]")
+    unsetSelectedValues("[name=department]")
+    setValue("#salary", "")
+    setValue("#notes", "")
+    setValue("#day","1")
+    setValue("#month","January")
+    setValue("#year", "2020")
+}
