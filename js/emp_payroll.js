bd13bfaed462a071d6ed01a9244f737c372d5322
Soumikpal9 <soumikpal1997@gmail.com>
2020-11-21 23:36:46 +0530

    [SP]Added: Ability To Update The Data And Save It Into The Local Storage Using UI

--- js/emp_payroll.js
+++ js/emp_payroll.js
@@ -48,8 +48,13 @@ class EmployeePayrollData {
         return this._startDate
     }
     set startDate(startDate){
-        if(startDate <= new Date()) this._startDate = startDate
-        else throw "Enter valid date"
+        let now = new Date()
+        if(startDate > now) throw 'Start Date is a Future Date!'
+        var diff = Math.abs(now.getTime() - startDate.getTime())
+        if(diff / (1000 * 60 * 60 *24) > 30){
+            throw 'Start Date is beyond 30 Days!'
+        }
+        this._startDate = startDate
     }
 
     get note(){
