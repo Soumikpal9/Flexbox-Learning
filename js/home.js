918c23d802765c9962386627bdd5baf59beda0ad
Soumikpal9 <soumikpal1997@gmail.com>
2020-11-21 22:39:39 +0530

    [SP]Added: Ability To Update The Data From The Local Storage Using UI

--- js/home.js
+++ js/home.js
@@ -24,8 +24,8 @@ const createInnerHtml = () => {
             <td>${empPayrollData._salary}{</td>
             <td>${stringifyDate(empPayrollData._startDate)}</td>
             <td>
-                <img id="${empPayrollData._name}" onclick="remove(this)" alt="delete" src="../assets/delete_icon.png">
-                <img id="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/edit_icon.png">
+                <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/delete_icon.png">
+                <img id="${empPayrollData._name}" onclick="update(this)" alt="edit" src="../assets/edit_icon.png">
             </td>
         </tr>
         `
@@ -45,11 +45,18 @@ const getEmployeePayrollDataFromStorage = () => {
 }
 
 const remove = (node) => {
-    let empPayrollData = empPayrollList.find(empData => empData._name == node.id)
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id)
     if(!empPayrollData) return
-    const index = empPayrollList.map(empData => empData._name).indexOf(empPayrollData._name)
+    const index = empPayrollList.map(empData => empData._id).indexOf(empPayrollData._id)
     empPayrollList.splice(index, 1)
     localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList))
     document.querySelector(".emp-count").textContent = empPayrollList.length
     createInnerHtml()
+}
+
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._name == node.id)
+    if(!empPayrollData) return
+    localStorage.setItem('editEmp', JSON.stringify(empPayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page)
 }
